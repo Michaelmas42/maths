1cd4035cae24d43b686c2d6ec77554c2b896d690
michaelmas42 <michaelmas42@gmail.com>
2024-12-27 21:16:28 -0500

    whole number division and error checking

--- maths.c
+++ maths.c
@@ -3,12 +3,12 @@
 #include <time.h>
 #include <math.h>
 
-typedef enum {
-	ADD = 0,
-	SUBTRACT = 1,
-	MULTIPLY = 2,
-	DIVIDE = 3
-} op;
+#define BUFSIZE 256
+
+#define	ADD 0
+#define	SUBTRACT 1
+#define	MULTIPLY 2
+#define	DIVIDE 3
 
 void wait() {
 	char c;
@@ -18,39 +18,57 @@ void wait() {
 }	
 
 int rand_int(int digits) {
-	int n = rand() % (int) pow(10, digits);
-	return n + (int) pow(10, digits-1);
+	int lo = pow(10, digits-1);
+	int hi = pow(10, digits) - lo;
+	return rand() % hi + lo;
 }
+
+void set_answer(char* buffer, const char* fmt, int value) {
+	int n = snprintf(buffer, BUFSIZE, fmt, value);
+	if (n < 0) {
+		fprintf(stderr, "snprintf error");
+		exit(1);
+	}
+	if (n >= BUFSIZE) {
+		fprintf(stderr, "answer truncated");
+	}
+}	
 
 int main(void) {
 
 	srand(time(NULL));
 	char* pattern = "%i %c %i = ";
 
-	char answer[256];
+	char answer[BUFSIZE];
 	char symbol;
 
+	int a, b, operation;
+
 	while (1) {
-		int operation = rand() % 4;
-		int a = rand_int(3); 
-		int b = rand_int(2); 
+		operation = rand() % 4;
+		a = rand_int(3); 
+		b = rand_int(2); 
 	
 		switch(operation) {
 			case ADD:
 				symbol = '+';
-				sprintf(answer, "%i", a+b);
+				set_answer(answer, "%i", a+b);
 				break;	
 			case SUBTRACT:
 				symbol = '-';
-				sprintf(answer, "%i", a-b);
+				set_answer(answer, "%i", a-b);
 				break;	
 			case MULTIPLY:
 				symbol = '*';
-				sprintf(answer, "%i", a*b);
+				set_answer(answer, "%i", a*b);
 				break;	
 			case DIVIDE:
 				symbol = '/';
-				sprintf(answer, "%.4f", (float)a/b);
+				while (a % b != 0) {
+					a = rand_int(3); 
+					b = rand_int(2); 
+				}
+				set_answer(answer, "%i", a/b);
 				break;	
 		}	
 		printf(pattern, a, symbol, b);
